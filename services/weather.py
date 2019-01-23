4dd0f83cfd4bbfa9e9b34210bb96f20c53131daf
Ahmed Loay <ahmed@shahwan.me>
2019-01-23 16:13:49 +0200

    cleaner code

--- services/weather.py
+++ services/weather.py
@@ -1,17 +1,13 @@
-
 import requests
-from bs4 import BeautifulSoup   
-import html
+from bs4 import BeautifulSoup
 
 
-def weather() :
+def weather():
     page = requests.get(
-    	'https://www.google.com/search',
-    	params={'q': 'weather+zagazig', 'oq':'weather'})
+        'https://www.google.com/search',
+        params={'q': 'weather+zagazig', 'oq': 'weather'})
 
-    soup = BeautifulSoup(page.content , 'html.parser')
+    soup = BeautifulSoup(page.content, 'html.parser')
     temp = soup.findAll('span', {'class': 'wob_t'})
     temperature = temp[0].text
     return temperature
-
-
