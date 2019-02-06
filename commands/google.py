06cce3ea1d7bb6af29829d0e9b4c355996c13a3a
Ahmed Loay <ahmed@shahwan.me>
2019-02-06 12:39:46 +0200

    fix list-out-of-range error, google command should not fail anymore

--- commands/google.py
+++ commands/google.py
@@ -9,13 +9,17 @@ def google_search(message):
     links = soup.findAll('cite')  # links list
     descriptions = soup.findAll('span', {'class': 'st'})  # descriptions list
 
+    if len(links) == 0:  # if no results found
+        return 'No results found for your search query. Try another meaningfull one.'
+
     result = []  # result to be returned link+description
-    for i in range(9):
-        result.append(links[i].text)
-        result.append(descriptions[i].text)
+    for idx, link in enumerate(links):  # loop over the links (whatever their length is)
+        result.append(link.text)  # current link
+        result.append(descriptions[idx].text)  # current link description
+        result.append('\n')  # new line
 
     return '\n'.join(result)
 
 
 if __name__ == '__main__':
-    print(google_search('how are you'))
+    print(google_search('google me please'))
