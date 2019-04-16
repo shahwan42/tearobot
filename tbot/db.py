0dc03c4c79c20526d9eaa3fa64d157133ffef0c3
Ahmed Shahwan <ahmed@shahwan.me>
2019-04-16 15:45:27 +0200

    db setup() and destroy()

--- tbot/db.py
+++ tbot/db.py
@@ -1,6 +1,7 @@
 """
     Database management module
 """
+import os
 import sqlite3
 
 from sqlite3 import Error
@@ -18,6 +19,26 @@ class DBHelper():
     def __del__(self):
         """On object destruction"""
         self.conn.close()
+
+    def setup(self):
+        """Set up database for dev/test purpose or for first time use"""
+        try:
+            self.conn.executescript('../db/bot.db.sql')
+        except Error as err:
+            exit(err)
+        return "DB setup was successful"
+
+    def destroy(self):
+        try:
+            self.conn.execute('DROP TABLE User;')
+            self.conn.execute('DROP TABLE Message;')
+            self.conn.commit()
+            print('dropping tables... done.')
+            self.conn.close()
+            os.remove(path='../db/dev.db')
+            print('removed db file')
+        except Error as err:
+            exit(err)
 
     def insert(self, table: str, columns: tuple, values: tuple) -> bool:
         """Insert a new record into a table"""
@@ -56,5 +77,5 @@ if __name__ == "__main__":
     # Message: id, update_id, user_id, chat_id, date, text
     # User: id, is_bot, is_admin, first_name, last_name, username, language_code,
     # active, created, updated, last_command
-    db = DBHelper(filename='db/dev.db')
+    db = DBHelper(filename='../db/dev.db')
     print(db.retrieve('Message', ('*'), 'user_id > 2'))
