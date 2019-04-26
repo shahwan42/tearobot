27cabe82ea2133b342f6c0182d39acf64df1466d
Ahmed Shahwan <ahmed@shahwan.me>
2019-04-26 22:23:10 +0200

    added __str__ for each type

--- bot/data_types.py
+++ bot/data_types.py
@@ -6,19 +6,8 @@
 
 class User:
     """User type"""
-    def __init__(
-            self,
-            id: int,
-            is_bot: bool,
-            is_admin: bool,
-            first_name: str,
-            last_name: str,
-            username: str,
-            language_code: str,
-            active: bool,
-            created: int,
-            updated: int,
-            last_command: str):
+    def __init__(self, id: int, is_bot: bool, is_admin: bool, first_name: str, last_name: str, username: str,
+                 language_code: str, active: bool, created: int, updated: int, last_command: str):
         self.id = id
         self.is_bot = bool(is_bot)
         self.is_admin = bool(is_admin)
@@ -31,6 +20,12 @@ class User:
         self.updated = updated
         self.last_command = last_command
 
+    def __str__(self):
+        return f"[<User>: id: {self.id}, is_bot: {self.is_bot}, is_admin: {self.is_admin}, " \
+               f"first_name: {self.first_name}, last_name: {self.last_name}, username: {self.username}, " \
+               f"language_code: {self.language_code}, active: {self.active}, created: {self.created}, " \
+               f"updated: {self.updated}, last_command: {self.last_command}]"
+
 
 class Message:
     """Message type"""
@@ -42,6 +37,10 @@ class Message:
         self.date = date
         self.text = text
 
+    def __str__(self):
+        return f"[<Message>: id: {self.id}, update_id: {self.update_id}, user_id: {self.user_id}, " \
+               f"chat_id: {self.chat_id}, date: {self.date}, text: {self.text}]"
+
 
 class Event:
     """Event type"""
@@ -51,14 +50,15 @@ class Event:
         self.description = description
         self.cancelled = bool(cancelled)
 
+    def __str__(self):
+        return f"[<Event>: id: {self.id}, time: {self.time}, description: {self.description}, " \
+               f"cancelled: {self.cancelled}]"
+
 
 class ScheduleEntry:
     """Schedule Entry type"""
-    def __init__(
-            self,
-            id: int,
-            time: int,
-            saturday: str, sunday: str, monday: str, tuesday: str, wednesday: str, thursday: str):
+    def __init__(self, id: int, time: int, saturday: str, sunday: str, monday: str, tuesday: str,
+                 wednesday: str, thursday: str):
         self.id = id
         self.time = time
         self.saturday = saturday
@@ -67,3 +67,8 @@ class ScheduleEntry:
         self.tuesday = tuesday
         self.wednesday = wednesday
         self.thursday = thursday
+
+    def __str__(self):
+        return f"[<ScheduleEntry>: id: {self.id}, time: {self.time}, saturday: {self.saturday}, "\
+               f"sunday: {self.sunday}, monday: {self.monday}, tuesday: {self.tuesday}, "\
+               f"wednesday: {self.wednesday}, thursday: {self.thursday}]"
